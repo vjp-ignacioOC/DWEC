615f88019e9756af0dfba71d0cb3fea0c0616ab7
iormaechec01 <iormaechec01@informatica.iesvalledeljerteplasencia.es>
2023-10-06 20:31:43 +0200

    dwec

---
+++ UT2/EjerciciosFInalesTema01/Ejercicio06/js/main.js
@@ -0,0 +1,26 @@
+/*
+    Crea un array con 4 valores y realiza los siguientes pasos:
+    1) Añade dos elementos al inicio.
+    2) Añade 3 más al final.
+    3) Elimina las posiciones 3,4,5
+    4) Inserta 2 elementos antes del último elemento.
+    NOTA: En cada cambio muestra los elementos del array separados por una almohadilla
+    (no utilices bucles, utiliza las funciones predefinidas de arrays).
+ */
+
+arrayPrueba = [1,2,3,4];
+console.log("El Array inicial es: " + arrayPrueba);
+// 1
+arrayPrueba.unshift(5,6,7);
+console.log("La primera prueba es: " + arrayPrueba);
+// 2
+arrayPrueba.push(8,9,0);
+console.log("La segunda prueba es: " + arrayPrueba);
+//3
+arrayPrueba.splice(3,3);
+console.log("La tercera prueba es: " + arrayPrueba);
+// 4
+arrayPrueba.splice(-1,0,1, 2);
+console.log("La cuarta prueba es: " + arrayPrueba);
+
+
