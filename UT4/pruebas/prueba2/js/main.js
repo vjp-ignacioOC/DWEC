6265a491c325e59bd08a030018aa29317c9aeb42
iormaechec01 <iormaechec01@informatica.iesvalledeljerteplasencia.es>
2023-10-27 20:47:47 +0200

    dwec

---
+++ UT4/pruebas/prueba2/js/main.js
@@ -0,0 +1,28 @@
+class Usuario {
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.tipo = 1;
+    }
+
+    saludo() {
+        console.log(`Hola soy ${this.nombre}`);
+    }
+
+    decirTipo() {
+        console.log("Soy un usuario de "+this.tipo);
+    }
+}
+
+class Administrador extends Usuario {
+    constructor(nombre) {
+        super(nombre);
+        this.tipo = 2;
+    }
+
+    decirTipo() {
+        super.decirTipo();
+        console.log("Y también soy un administrador");
+    }
+}
+
+let admin = new Admini
