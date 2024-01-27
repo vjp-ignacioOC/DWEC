a6f3d7db0dae027ce92ecc32cf8c477054bfcc4d
vjp-ignacioOC <114687975+vjp-ignacioOC@users.noreply.github.com>
2024-01-27 19:21:59 +0100

    dwec

--- Ignacio_Ormaeche_Carral_Examen/Examen2/js/examen2-ajax.js
+++ Ignacio_Ormaeche_Carral_Examen/Examen2/js/examen2-ajax.js
@@ -46,59 +46,106 @@ function sacarInfo() {
 //En este método tenemos la funcionalidad de la página
 function mostrarInformacion(data) {
     //Primerocreamos un array de pasos y,mediante un foEach, creamos cad auno de los pasos que están en el Json
-    let pasosJson = [];
-    data.results.bindings.forEach(elemento => {
-        let paso = new Paso(elemento);
-        pasosJson.push(paso);
-    })
+    // let pasosJson = [];
+    // data.results.bindings.forEach(elemento => {
+    //     let paso = new Paso(elemento);
+    //     pasosJson.push(paso);
+    // })
 
     //Le damos funcionalidad a los diferentes botones
     bNombre.addEventListener('click', function() {
-        mostrarNombre(pasosJson);
+        mostrarNombre(data);
         //Con esto, hacemos que solose pueda dar albotón 1 vez, lo deshabilita
         this.disabled = true;
     });
     bFotos.addEventListener('click', function() {
-        mostrarNombreConImagen(pasosJson);
+        mostrarNombreConImagen(data);
         this.disabled = true;
     });
     bNombreFiltrado.addEventListener('click', function() {
-        mostrarNombresSegunParametro(pasosJson);
+        mostrarNombresSegunParametro(data);
         this.disabled = true;
-    })
+    });
 
 }
-
+function recorrerPasos(data) {
+    let pasosJson = [];
+    data.results.bindings.forEach(elemento => {
+        let paso = new Paso(elemento);
+        pasosJson.push(paso);
+    });
+    return pasosJson;
+}
 //hacemos un forEach para mostrar los nombres de los distintos pasos
 function mostrarNombre(data) {
-    data.forEach(element => {
+    let pasos = recorrerPasos(data);
+    pasos.forEach(element => {
         crearLi(element.getNombre(), cNombre);
     });
 }
 
 //Hacemos un forEach para mostrar el nombre y la imagen de los distintos pasos
 // No sé por qué, pero solo me muestra uno
+// function mostrarNombreConImagen(data) {
+//     data.forEach(element => {
+//         let paso = `<p> ${element.getNombre()} </p>
+// <img src="${element.getImg()}">`;
+//         cFotos.innerHTML = paso;
+//     });
+//
+// }
 function mostrarNombreConImagen(data) {
-    data.forEach(element => {
+    let pasos = recorrerPasos(data);
+    for (let i = 0; i < pasos.length; i++) {
+        let element = pasos[i];
         let paso = `<p> ${element.getNombre()} </p>
-<img src="${element.getImg()}">`;
-        cFotos.innerHTML = paso;
-    });
-
+                    <img src="${element.getImg()}">`;
+        cFotos.innerHTML += paso;  // Se utiliza '+=' para agregar a la propiedad innerHTML en lugar de sobrescribirla
+    }
 }
 
+
 // Hacemos que nos muestre losnombres ordenados alfabéticamente gracias al sort()
+// function mostrarNombresSegunParametro(data) {
+//     // Creamos un array, lo ordeamos,y luego creamos las distintas li
+//     let nombresPasos = [];
+//     data.forEach(element => {
+//         nombresPasos.push(element.getNombre());
+//     });
+//     nombresPasos.sort();
+//     for (let i = 0; i < nombresPasos.length; i++) {
+//         crearLi(nombresPasos[i], cNombreFiltrado);
+//     }
+// // Debería sacar los nombres qe le des según parámetro, perono sé cómo hacerlo, supongo que sería algo como if ( tNombreFiltrado.value === algo aquí) pues que lo muestre
+// }
 function mostrarNombresSegunParametro(data) {
-    // Creamos un array, lo ordeamos,y luego creamos las distintas li
+    // Creamos un array el cual contendrá todos los valores del Json que cumpla con el if que está a continuación
+    let arrayValores = [];
     let nombresPasos = [];
-    data.forEach(element => {
-        nombresPasos.push(element.getNombre());
-    });
-    nombresPasos.sort();
-    for (let i = 0; i < nombresPasos.length; i++) {
-        crearLi(nombresPasos[i], cNombreFiltrado);
+    // Recorremos el Json
+    for (let i = 0; i < data.results.bindings.length; i++) {
+        if (data.results.bindings[i].rdfs_label.value.toLowerCase().includes(tNombreFiltrado.value.toLowerCase())) {
+            arrayValores.push(data.results.bindings[i].rdfs_label.value); // Introducimos los nombres de los Pasos en el array.
+        }
     }
-// Debería sacar los nombres qe le des según parámetro, perono sé cómo hacerlo, supongo que sería algo como if ( tNombreFiltrado.value === algo aquí) pues que lo muestre
+
+    // Comprobamos que el array no está vacío. Si no lo está entramos en él.
+    if (arrayValores) {
+        // Ordenamos alfabéticamente el array con el método sort(), por defecto lo hará de la A-Z
+        arrayValores.sort();
+        // Nos recorremos el Array y llamamos a la función crearLi()
+        arrayValores.forEach(element => {
+            crearLi(element, cNombreFiltrado); // Le pasamos el elemento (nombre del Paso) y el contenedor donde se mostrará
+        });
+    } else {
+        data.forEach(element => {
+            nombresPasos.push(element.getNombre());
+        });
+        nombresPasos.sort();
+        for (let i = 0; i < nombresPasos.length; i++) {
+            crearLi(nombresPasos[i], cNombreFiltrado);
+        }
+    }
 }
 
 // Esta función sirve para crear los diferentes li de los nombres de los pasos
